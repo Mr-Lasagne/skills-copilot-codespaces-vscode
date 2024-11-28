8ed1008a554979eb9abccfc15243280d93bef7bb
Jack <58007311+Mr-Lasagne@users.noreply.github.com>
2024-11-28 17:02:57 +0000

    Edit member.js

--- member.js
+++ member.js
@@ -1,11 +1,21 @@
-function skillsHeader(){
-    var skills = document.getElementsByClassName("skills");
-    var header = document.getElementsByClassName("header");
-    var i;
-    for (i = 0; i < skills.length; i++) {
-        skills[i].style.display = "block";
-    }
-    for (i = 0; i < header.length; i++) {
-        header[i].style.display = "none";
-    }
+function skillsMember() {
+  // This function is called when the member page is loaded
+  // It fetches the member's skills and displays them in a list
+  // The list is displayed in a div with the id "skills"
+  let skills = [
+    "JavaScript",
+    "HTML",
+    "CSS",
+    "Python",
+    "React",
+    "Node.js",
+    "Express",
+    "MongoDB",
+  ];
+  let skillsDiv = document.getElementById("skills");
+  for (let i = 0; i < skills.length; i++) {
+    let skill = document.createElement("li");
+    skill.textContent = skills[i];
+    skillsDiv.appendChild(skill);
+  }
 }
